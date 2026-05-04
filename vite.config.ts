3e3c14b8773f496e85dc8a05bad110d17e55e4b4
Fabian-Petersen <fpetersen2@gmail.com>
2026-05-04 11:31:11 +0200

    restore vite.config & tconfig to resolve build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,45 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-// import path from "path";
-import { fileURLToPath, URL } from "url";
-
-// $ analyser used for performance optimisation
-import { visualizer } from "rollup-plugin-visualizer";
+import path from "path";
 
 export default defineConfig({
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    sourcemap: true, //default:false
-    minify: true, // default: "esbuild"
-    cssMinify: true,
-  },
-  plugins: [
-    react(),
-    tailwindcss(),
-    visualizer({
-      filename: "stats.html",
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-      template: "treemap", // or 'sunburst', 'network'
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
+
+// // import path from "path";
+// import { fileURLToPath, URL } from "url";
+
+// // $ analyser used for performance optimisation
+// import { visualizer } from "rollup-plugin-visualizer";
+
+// export default defineConfig({
+//   build: {
+//     outDir: "dist",
+//     emptyOutDir: true,
+//     sourcemap: true, //default:false
+//     minify: true, // default: "esbuild"
+//     cssMinify: true,
+//   },
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     visualizer({
+//       filename: "stats.html",
+//       open: false,
+//       gzipSize: true,
+//       brotliSize: true,
+//       template: "treemap", // or 'sunburst', 'network'
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//     },
+//   },
+// });
